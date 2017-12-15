6f1b2e54a3ed540e348bc91616cb32e96fd61591
Kulik Maksim <kulikmaxim92@gmail.com>
2017-12-15 23:42:49 +0300

    fix some problems (#5)

--- src/components/Table/TableItem.js
+++ src/components/Table/TableItem.js
@@ -6,7 +6,7 @@ function TableItem(props) {
         <td><input type="checkbox" checked={props.done} onChange={() => props.toggleChecked(props.id)}/></td>
         <td title={props.description}>{props.title}</td>
         <td>{props.importanceNames[props.importance]}</td>
-        <td>{props.date.getDay()}</td>
+        <td>{`${props.date.getDate()}.${props.date.getMonth() + 1}.${props.date.getFullYear()}`}</td>
     </tr>
     );
 }
